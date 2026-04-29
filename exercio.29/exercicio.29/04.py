d26887e253357654564659fb900ae1674f032e2c
patrickhenrique3tjf-beep <patrickhenrique3tjf@gmail.com>
2026-04-29 19:40:06 -0300

    Add comparison logic for three input numbers

---
+++ exercio.29/exercicio.29/04.py
@@ -0,0 +1,9 @@
+n1 = int(input("2"))
+n2 = int(input("4"))
+n3 = int(input("8"))
+
+if n1 > n2 and n1 > n3:
+    print("primeiro numero e maior")
+elif n2 > n3:
+    print("segundo numero e maior")
+    
